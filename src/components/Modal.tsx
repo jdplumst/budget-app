675e83c84773569609704631d7877f55d20db286
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-07-02 17:07:53 -0400

    Add modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface IModal {
+  children: ReactNode;
+}
+
+export default function Modal({ children }: IModal) {
+  return (
+    <div className="absolute bottom-0 top-0 z-10 h-screen w-screen bg-white/50">
+      <div className="absolute left-1/2 top-1/2 z-10 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center bg-black p-4 text-white opacity-100">
+        {children}
+      </div>
+    </div>
+  );
+}
