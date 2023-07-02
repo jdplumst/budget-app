cb0ca8892782960ae1f3f5a03850199e0be3a09f
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-07-02 16:21:29 -0400

    Install react-icons

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -3,7 +3,9 @@ import Navbar from "@/components/Navbar";
 import { api } from "@/constants";
 import useSession from "@/hooks/useSession";
 import Router from "next/router";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
+import { GrUpdate } from "react-icons/gr";
+import { AiFillDelete } from "react-icons/ai";
 
 export default function Projects() {
   const { user, userLoading } = useSession();
@@ -29,6 +31,35 @@ export default function Projects() {
     { enabled: !!user }
   );
 
+  const updateProject = async () => {
+    const response = await fetch(`${api}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      // body: JSON.stringify({ username, password }),
+      credentials: "include"
+    });
+    if (!response.ok) {
+      const error = await response.text();
+      throw Error(error);
+    }
+    return;
+  };
+
+  const { mutate: updateMutation, isLoading: updateLoading } = useMutation(
+    updateProject,
+    {
+      onSuccess(data, variables, context) {
+        // Router.push("/projects");
+      },
+      onError(error, variables, context) {
+        // setError((error as Error).message);
+        // setDisabled(false);
+      }
+    }
+  );
+
   if (userLoading || projectsLoading)
     return (
       <div className="flex h-screen items-center justify-center">
@@ -52,8 +83,14 @@ export default function Projects() {
           <div
             key={2}
             className="relative flex h-72 w-72 justify-center border-2 border-solid border-black text-center text-4xl font-bold">
+            <div className="absolute bottom-2 left-5 hover:cursor-pointer">
+              <GrUpdate size={20} />
+            </div>
+            <div className="absolute bottom-2 right-5 hover:cursor-pointer">
+              <AiFillDelete size={20} />
+            </div>
             <div className="absolute top-10">{p.name}</div>
-            <button className="absolute bottom-10 w-40 rounded-lg border-2 border-black bg-green-500 p-2 font-bold hover:bg-green-600">
+            <button className="absolute bottom-12 w-40 rounded-lg border-2 border-black bg-green-500 p-2 font-bold hover:bg-green-600">
               Select
             </button>
           </div>
