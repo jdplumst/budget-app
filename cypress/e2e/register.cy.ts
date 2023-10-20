775b9edd1b56e29586d81aebe234096cecbdd4d1
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-10-20 19:12:39 -0400

    Add register test for existing username

--- cypress/e2e/register.cy.ts
+++ cypress/e2e/register.cy.ts
@@ -3,21 +3,32 @@ describe("Register Tests", () => {
     cy.visit("/");
     cy.getDataTest("signup-link").click();
   });
+
   it("Blank username and password", () => {
     cy.contains("Must enter a username").should("not.exist");
     cy.getDataTest("signup-button").click();
     cy.contains("Must enter a username").should("be.visible");
   });
+
   it("Blank username", () => {
     cy.getDataTest("signup-password-input").type("x");
     cy.contains("Must enter a username").should("not.exist");
     cy.getDataTest("signup-button").click();
     cy.contains("Must enter a username").should("be.visible");
   });
+
   it("Blank password", () => {
     cy.getDataTest("signup-username-input").type("x");
     cy.contains("Must enter a password").should("not.exist");
     cy.getDataTest("signup-button").click();
     cy.contains("Must enter a password").should("be.visible");
+  });
+
+  it("Existing username", () => {
+    cy.getDataTest("signup-username-input").type(Cypress.env("guest").username);
+    cy.getDataTest("signup-password-input").type("x");
+    cy.contains("Username already taken").should("not.exist");
+    cy.getDataTest("signup-button").click();
+    cy.contains("Username already taken").should("be.visible");
   });
 });
